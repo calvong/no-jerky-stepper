b6348b342f473c8592a1a6ee1d4ed19d3eb29fa8
calvong <calvongchi@gmail.com>
2024-07-31 23:21:38 +1000

    bug fixes

--- no_jerky_stepper.h
+++ no_jerky_stepper.h
@@ -1,6 +1,7 @@
 #ifndef NO_JERKY_STEPPER_H
 #define NO_JERKY_STEPPER_H
 
+#include "platform.h"
 
 
 #endif
--- platform.h
+++ platform.h
@@ -1,10 +1,15 @@
 #ifndef NO_JERKY_PLATFORM_H
 #define NO_JERKY_PLATFORM_H
 
-#include <cstdint>
+#include <stdint.h>
 
-void no_jerky_step();
-void no_jerky_dir(uint8_t dir);
+typedef struct no_jerky_motor_pins
+{
+    uint8_t dir_pin;
+    uint8_t step_pin;
+}no_jerky_motor_pins_t;
+
+void no_jerky_step(no_jerky_motor_pins_t motor_pins, uint8_t dir);
 void no_jerky_delay_ms(uint16_t ms);
 void no_jerky_isr();
 
